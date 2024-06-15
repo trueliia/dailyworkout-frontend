eb0195315833dcd724e1c5854c9d85ca1c7ea6fa
Julia Naumova <140359113+trueliia@users.noreply.github.com>
2024-06-15 11:44:10 -0500

    Update MyWorkouts.js
    adapt for smaller screens

--- src/MyWorkouts.js
+++ src/MyWorkouts.js
@@ -3,8 +3,10 @@ import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 export const MyWorkouts = ({ text, updatingInInput, deleteWorkout}) =>{
     return (<div className="icons">
         <p> { text }</p>
+        <div className="makeChanges">
         <AiFillEdit onClick={updatingInInput}></AiFillEdit>
         <AiFillDelete onClick={deleteWorkout}></AiFillDelete>
+        </div>
     </div>
     )
 }
